2515d620191bae69925ffaf479ad0a3d6f5529a4
Gig77 <frech.christian@gmail.com>
2015-11-27 13:31:48 +0100

    label not only top-n significant genes but also top-n in terms of fold-change

--- components/VolcanoPlot/main.r
+++ components/VolcanoPlot/main.r
@@ -18,6 +18,7 @@ execute <- function(cf) {
 	geneNameCol <- get.parameter(cf, 'geneNameCol', 'int')
 	labelTopN <- get.parameter(cf, 'labelTopN', 'int')
 	legendpos <- get.parameter(cf, 'legendpos', 'string')
+	excludeGenes <- get.parameter(cf, 'excludeGenes', 'string')
 	caption <- get.parameter(cf, 'caption', 'string')
 	sampleSize <- get.parameter(cf, 'sampleSize', 'int')
 	cexLabel <- get.parameter(cf, 'cexLabel', 'float')
@@ -42,6 +43,11 @@ execute <- function(cf) {
 	res.uninteresting <- subset(res, padj > sigthresh & abs(log2FoldChange) < lfcthresh)
 	res.uninteresting.sample <- res.uninteresting[sample(1:nrow(res.uninteresting), min(sampleSize, nrow(res.uninteresting)), replace = F),]
   	res.sample <- rbind(res.interesting, res.uninteresting.sample)
+	
+	if (excludeGenes != "") {
+		exclude <- unlist(strsplit(excludeGenes,','))
+		res.sample <- res.sample[!res.sample$Name %in% exclude,]
+	}
   
 	# prepare document
 	tex <- character(0)
@@ -64,7 +70,7 @@ execute <- function(cf) {
 	# plot
 	pdf(file.path(out.dir, plot.file))
 	with(subset(res.sample, padj >  sigthresh & abs(log2FoldChange) <  lfcthresh), plot(log2FoldChange, -log10(pvalue), pch=20, col="lightgray", xlim=c(min(res.sample$log2FoldChange)-1,max(res.sample$log2FoldChange)+1), ylim=range(-log10(res.sample$pvalue)), cex=cexPoint, main=""))
-  with(subset(res.sample, padj <= sigthresh & abs(log2FoldChange) <  lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="black", cex=cexPoint))
+    with(subset(res.sample, padj <= sigthresh & abs(log2FoldChange) <  lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="black", cex=cexPoint))
 	with(subset(res.sample, padj >  sigthresh & abs(log2FoldChange) >= lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="blue", cex=cexPoint))
 	with(subset(res.sample, padj <= sigthresh & abs(log2FoldChange) >= lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="red", cex=cexPoint))
 
@@ -72,15 +78,30 @@ execute <- function(cf) {
 	if (labelTopN > 0) {
     library(calibrate)
     
-  	res.sample.sorted <- res.sample[order(res.sample$padj, -abs(res.sample$log2FoldChange)),]
+  	res.sample.sorted <- res.sample[order(res.sample$padj),]
   	res.sample.sorted <- res.sample.sorted[!is.na(res.sample.sorted$padj) & res.sample.sorted$log2FoldChange > 0,]
-  	with(res.sample.sorted[1:min(labelTopN, nrow(res.sample.sorted)),], textxy(log2FoldChange, -log10(pvalue), labs=Name, cex=cexLabel, offset=0.7))
+	label.up.sig <- res.sample.sorted$id[1:min(labelTopN, nrow(res.sample.sorted))]
 
-  	res.sample.sorted <- res.sample[order(res.sample$padj, -abs(res.sample$log2FoldChange)),]
+	res.sample.sorted <- res.sample[order(-res.sample$log2FoldChange),]
+	res.sample.sorted <- res.sample.sorted[!is.na(res.sample.sorted$padj) & res.sample.sorted$log2FoldChange > 0,]
+	label.up.fc <- res.sample.sorted$id[1:min(labelTopN, nrow(res.sample.sorted))]
+	
+	with(res.sample.sorted[res.sample.sorted$id %in% c(label.up.sig, label.up.fc),], textxy(log2FoldChange, -log10(pvalue), labs=Name, cex=cexLabel, offset=0.7))
+
+  	res.sample.sorted <- res.sample[order(res.sample$padj),]
   	res.sample.sorted <- res.sample.sorted[!is.na(res.sample.sorted$padj) & res.sample.sorted$log2FoldChange < 0,]
-  	with(res.sample.sorted[1:min(labelTopN, nrow(res.sample.sorted)),], textxy(log2FoldChange, -log10(pvalue), labs=Name, cex=cexLabel, offset=0.7))
+	label.dn.sig <- res.sample.sorted$id[1:min(labelTopN, nrow(res.sample.sorted))]
+
+	res.sample.sorted <- res.sample[order(res.sample$log2FoldChange),]
+	res.sample.sorted <- res.sample.sorted[!is.na(res.sample.sorted$padj) & res.sample.sorted$log2FoldChange < 0,]
+	label.dn.fc <- res.sample.sorted$id[1:min(labelTopN, nrow(res.sample.sorted))]
+	
+  	with(res.sample.sorted[res.sample.sorted$id %in% c(label.dn.sig, label.dn.fc),], textxy(log2FoldChange, -log10(pvalue), labs=Name, cex=cexLabel, offset=0.7))
   	
-  	caption <- paste0(caption, " The top-", labelTopN, " most significant up- and down-regulated genes are labeled. P-values were capped at ", sprintf("%.1g", minP), ".")
+  	caption <- paste0(caption, " The top-", labelTopN, " most significant up- and down-regulated genes (either in terms of q-value or fold-change) are labeled. P-values were capped at ", sprintf("%.1g", minP), ".")
+	if (excludeGenes != "") {
+		caption <- paste0(caption, " The following genes are not shown: ", excludeGenes, ".")
+	}
   }
   legend(legendpos, xjust=1, yjust=1, legend=c(paste("FDR<=",sigthresh,sep=""), paste("|LogFC|>=",lfcthresh,sep=""), "both"), pch=20, col=c("black","blue","red"))
   dev.off()
