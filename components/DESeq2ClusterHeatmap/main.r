6ed3c70190e9dea14b482d4148e2831fd549ca79
Gig77 <frech.christian@gmail.com>
2015-11-27 21:45:24 +0100

    take count matrix instead of count file array as input; output normalized count matrices on which clustering is based

--- components/DESeq2ClusterHeatmap/main.r
+++ components/DESeq2ClusterHeatmap/main.r
@@ -3,10 +3,14 @@ library(componentSkeleton)
 execute <- function(cf) {
 
 	# enable for debugging
-	#cf <- parse.command.file("/tmp/execute/DESEq2ClusterHeatmap/case1/component/_command")
+	#cf <- parse.command.file("/mnt/projects/martin/results/anduril/execute/qcReport-samplesClusterHeatmap/_command")
 	
 	# Meta data
 	instance.name <- get.metadata(cf, 'instanceName')	
+
+	# Inputs
+	countMatrix  <- Matrix.read(get.input(cf, "countMatrix"))
+	samples      <- CSV.read(get.input(cf, 'samples'))
 	
 	# Params
 	caption <- get.parameter(cf, 'caption',    type = 'string')
@@ -16,15 +20,15 @@ execute <- function(cf) {
 	do.rlog <- get.parameter(cf, 'rlog', type = 'boolean')
 	do.vst <- get.parameter(cf, 'vst', type = 'boolean')
 	do.voom <- get.parameter(cf, 'voom', type = 'boolean')
-	
-	# Inputs
-	sampleTable  <- Array.read(cf,"geneCounts")
-	
+			
 	library("DESeq2")
 	library("RColorBrewer")
 	library("gplots")
 	
-	cds <- DESeqDataSetFromHTSeqCount(sampleTable=sampleTable, directory="/", design=~1)
+	rownames(samples) <- samples$Alias
+	samples <- samples[samples$Alias %in% colnames(countMatrix),]
+	countMatrix <- countMatrix[,rownames(samples)]
+	cds <- DESeqDataSetFromMatrix(countData = countMatrix, colData = samples, design = ~1)	
 	expressed <- rowSums(counts(cds)) >= 10
 	caption <- paste(caption, "The clustering is based on", sum(expressed), "expressed genes.", sum(!expressed), "genes were excluded because they had less than ten reads in total across all samples.")
 	hmcol <- colorRampPalette(brewer.pal(10, "RdBu"))(256)
@@ -35,6 +39,7 @@ execute <- function(cf) {
 	# rlog
 	if (do.rlog) {
 		rld <- rlog(cds[expressed,])
+		CSV.write(get.output(cf, 'rlog'), assay(rld), first.cell = "Ensembl")
 		#dists.rld <- as.matrix(dist(t(assay(rld))))
 		dists.rld <- as.matrix(as.dist(1-cor(assay(rld), method="spearman"))) # spearman correlation
 		
@@ -42,18 +47,22 @@ execute <- function(cf) {
 		pdf(file.path(report.dir, plot.file.rld))
 		heatmap.2(dists.rld, trace="none", scale="none", col=rev(hmcol), margin=c(7, 7), cexRow=cexRow, cexCol=cexRow, key.title="")
 		dev.off()
+	} else {
+	  file.create(get.output(cf, 'rlog'))
 	}
-
+	
 	# VST
 	if (do.vst) {
-		vsd <- varianceStabilizingTransformation(cds[expressed,])
-		#dists.vsd <- as.matrix(dist(t(assay(vsd))))
-		dists.vsd <- as.matrix(as.dist(1-cor(assay(vsd), method="spearman"))) # spearman correlation
+		vst <- varianceStabilizingTransformation(cds[expressed,])
+		CSV.write(get.output(cf, 'vst'), assay(vst), first.cell = "Ensembl")
+		dists.vst <- as.matrix(as.dist(1-cor(assay(vst), method="spearman"))) # spearman correlation
 		
 		plot.file.vst <- sprintf('%s-heatmap-vst.pdf', instance.name)
 		pdf(file.path(report.dir, plot.file.vst))
-		heatmap.2(dists.vsd, trace="none", scale="none", col=rev(hmcol), margin=c(7, 7), cexRow=cexRow, cexCol=cexRow, key.title="")
+		heatmap.2(dists.vst, trace="none", scale="none", col=rev(hmcol), margin=c(7, 7), cexRow=cexRow, cexCol=cexRow, key.title="")
 		dev.off()
+	} else {
+	  file.create(get.output(cf, 'vst'))
 	}
 	
 	# voom
@@ -63,12 +72,15 @@ execute <- function(cf) {
 		dge.norm <- calcNormFactors(dge, method="TMM")
 		y <- voom(dge.norm)
 		#dists.voom <- as.matrix(dist(t(y$E)))
+		CSV.write(get.output(cf, 'voom'), y$E, first.cell = "Ensembl")
 		dists.voom <- as.matrix(as.dist(1-cor(y$E, method="spearman"))) # spearman correlation
 		
 		plot.file.voom <- sprintf('%s-heatmap-voom.pdf', instance.name)
 		pdf(file.path(report.dir, plot.file.voom))
 		heatmap.2(dists.voom, trace="none", scale="none", col=rev(hmcol), margin=c(7, 7), cexRow=cexRow, cexCol=cexRow, key.title="")
 		dev.off()
+	} else {
+	  file.create(get.output(cf, 'voom'))
 	}
 	
 	# prepare Latex document
