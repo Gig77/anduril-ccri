537fc15c60d96ddda158164e46b381556a9b461a
Gig77 <frech.christian@gmail.com>
2016-01-18 13:22:52 +0100

    bugfix: don't choke on empty enrichment table

--- components/GSEA/annotate-genesets.r
+++ components/GSEA/annotate-genesets.r
@@ -12,7 +12,7 @@ opt <- parse_args(OptionParser(option_list=option_list))
 
 # debug
 # opt <- data.frame('gsea-result-file' = "/mnt/projects/iamp/results/anduril/execute/GSEA/case1/component/enrichedUp.csv", 'phenotype' = "up", 'gene-set-dir' = "/mnt/projects/iamp/results/anduril/execute/GSEA/case1/component/gsea_output", 'num-genes' = 87, 'annotations' = '/mnt/projects/iamp/results/geneset_category.tsv', stringsAsFactors=F, check.names=F)   
-
+# opt <- data.frame('gsea-result-file' = "/mnt/projects/ikaros/results/anduril/execute/gseaIKAROSUnidirectional_IKNp_vs_IKNx/enrichedDown.csv.part", 'phenotype' = "up", 'gene-set-dir' = "/mnt/projects/ikaros/results/anduril/execute/gseaIKAROS_IKNp_vs_IKNx/gsea_output", 'num-genes' = 30441, 'annotations' = "/mnt/projects/generic/data/ccri/geneset_annotation.tsv", 'output-file' = "/mnt/projects/ikaros/results/anduril/execute/gseaIKAROS_IKNp_vs_IKNx/enrichedUp.csv", stringsAsFactors=F, check.names=F)
 stopifnot(opt$phenotype == 'up' || opt$phenotype == 'down')
 
 print(paste("Adding core gene names with ranks to file", opt$'gsea-result-file', "..."))
@@ -20,55 +20,57 @@ print(paste("Adding core gene names with ranks to file", opt$'gsea-result-file',
 # read GSEA results
 gsea.result <- read.delim(opt$'gsea-result-file', stringsAsFactors = FALSE, check.names = FALSE)
 out <- gsea.result[,!names(gsea.result) %in% c("")] # drop extra empty column
-out$CORE_GENES <- NA
+out <- cbind(out, data.frame(CORE_GENES=as.character(rep(NA, nrow(out))))) ; out$CORE_GENES <- as.character(out$CORE_GENES)
 
 # read gene set to category assignments (if provided)
-if (!is.null(opt$annotations) && !is.na(opt$annotations) && nchar(opt$annotations) > 0) {
+if (nrow(out) > 0 && !is.null(opt$annotations) && !is.na(opt$annotations) && nchar(opt$annotations) > 0) {
   annotations <- read.delim(opt$annotations, stringsAsFactors = FALSE)
   names(annotations)[2] <- "CATEGORY"
   names(annotations)[3] <- "DESCRIPTION"
   names(annotations)[4] <- "LINKOUT"
   out <- merge(out, annotations, by.x="NAME", by.y="geneset", all.x=T)
 } else {
-	out$CATEGORY <- NA
-	out$DESCRIPTION <- NA
-	out$LINKOUT <- NA
+	out <- cbind(out, data.frame(CATEGORY=as.character(rep(NA, nrow(out))))) ; out$CATEGORY <- as.character(out$CATEGORY)
+	out <- cbind(out, data.frame(DESCRIPTION=as.character(rep(NA, nrow(out))))) ; out$DESCRIPTION <- as.character(out$DESCRIPTION)
+	out <- cbind(out, data.frame(LINKOUT=as.character(rep(NA, nrow(out))))) ; out$LINKOUT <- as.character(out$LINKOUT)
 }
 
-
-# for each significant gene set, load gene set result file and determine core enrichment gene names
-processed <- 0
-skipped <- 0
-for (i in 1:nrow(gsea.result)) {
-  gs.name <- gsea.result$NAME[i]
-  gs.genes.file <- paste0(opt$'gene-set-dir', "/", gs.name, ".xls")
-  if (!file.exists(gs.genes.file)) {
-    #print(paste("Output file for gene set ", gs.name, "not found. SKIPPED!"))
-    skipped <- skipped + 1
-    next
-  }
-  #print(paste("Adding ", i, "of", nrow(gsea.result), "gene sets:", gs.name))
-  gs.genes <- read.delim(gs.genes.file, row.names = 1, stringsAsFactors = FALSE)
-  gs.genes <- gs.genes[gs.genes$CORE.ENRICHMENT == "Yes",]
-  
-  if (opt$phenotype == 'up') {
-    gs.genes$rank <- gs.genes$RANK.IN.GENE.LIST + 1
-  } else {
-    gs.genes$rank <- opt$'num-genes' - gs.genes$RANK.IN.GENE.LIST
-  }
-  gs.genes <- gs.genes[order(gs.genes$rank),]
-  gs.genes$CORE_GENES <- paste0(gs.genes$PROBE, "(", gs.genes$rank, ")")
-  collapsed <- paste(gs.genes$CORE_GENES, collapse=", ")
-  out$CORE_GENES[out$NAME==gs.name] <- collapsed
-
-  processed <- processed + 1
+if (nrow(out) > 0) {
+	
+	# for each significant gene set, load gene set result file and determine core enrichment gene names
+	processed <- 0
+	skipped <- 0
+	for (i in 1:nrow(gsea.result)) {
+		gs.name <- gsea.result$NAME[i]
+		gs.genes.file <- paste0(opt$'gene-set-dir', "/", gs.name, ".xls")
+		if (!file.exists(gs.genes.file)) {
+			#print(paste("Output file for gene set ", gs.name, "not found. SKIPPED!"))
+			skipped <- skipped + 1
+			next
+		}
+		#print(paste("Adding ", i, "of", nrow(gsea.result), "gene sets:", gs.name))
+		gs.genes <- read.delim(gs.genes.file, row.names = 1, stringsAsFactors = FALSE)
+		gs.genes <- gs.genes[gs.genes$CORE.ENRICHMENT == "Yes",]
+		
+		if (opt$phenotype == 'up') {
+			gs.genes$rank <- gs.genes$RANK.IN.GENE.LIST + 1
+		} else {
+			gs.genes$rank <- opt$'num-genes' - gs.genes$RANK.IN.GENE.LIST
+		}
+		gs.genes <- gs.genes[order(gs.genes$rank),]
+		gs.genes$CORE_GENES <- paste0(gs.genes$PROBE, "(", gs.genes$rank, ")")
+		collapsed <- paste(gs.genes$CORE_GENES, collapse=", ")
+		out$CORE_GENES[out$NAME==gs.name] <- collapsed
+		
+		processed <- processed + 1
+	}
+	
+	print(paste("Processed", processed, "gene sets."))
+	if (skipped > 0) print(paste("WARNING: Skipped", skipped, "gene sets because output file was not found. Consider increasing '-plot_top_x' value when running GSEA."))
+	
+	# sort
+	out <- out[order(abs(out$NES), decreasing = TRUE),]
 }
-
-print(paste("Processed", processed, "gene sets."))
-if (skipped > 0) print(paste("WARNING: Skipped", skipped, "gene sets because output file was not found. Consider increasing '-plot_top_x' value when running GSEA."))
-
-# sort
-out <- out[order(abs(out$NES), decreasing = TRUE),]
 
 # write output
 write.table(out[,c("NAME", "DESCRIPTION", "CATEGORY", "LINKOUT", "SIZE", "ES", "NES", "NOM p-val", "FDR q-val", "FWER p-val", "RANK AT MAX", "LEADING EDGE", "CORE_GENES")], opt$'output-file', quote=TRUE, sep="\t",  na="", row.names=FALSE, col.names=TRUE)
